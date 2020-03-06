f30c5f5da77c68b7622abba3138d3e2c5e1acff3
gheorghestefan996 <gheorghe.stefan996@gmail.com>
2020-03-06 14:41:25 +0200

    added tb and monitor file

---
+++ Multiplicator numere complexe/complex_nr_multiplier_1_instance/src/tb/clock_rst_gen.v
@@ -0,0 +1,9 @@
+// Module:  clock_rst_gen
+// Author:  Gheorghe Stefan
+// Date:    06.03.2020
+
+module clock_rst_gen(
+    
+);
+
+endmodule // clock_rst_gen
---
+++ Multiplicator numere complexe/complex_nr_multiplier_1_instance/src/tb/tb.v
@@ -0,0 +1,9 @@
+// Module:  complex_nr_mult_tb
+// Author:  Gheorghe Stefan
+// Date:    06.03.2020
+
+module complex_nr_mult_tb(
+    
+);
+
+endmodule // complex_nr_mult_tb
