fe5b307d2e07680bbda531835d83eeda58f9de8d
gheorghestefan996 <gheorghe.stefan996@gmail.com>
2020-04-01 18:26:05 +0300

    all modules working on first test scenario

--- Multiplicator numere complexe/complex_nr_multiplier_4_instances/src/tb/complex_nr_mult_tb.v
+++ Multiplicator numere complexe/complex_nr_multiplier_4_instances/src/tb/complex_nr_mult_tb.v
@@ -51,7 +51,7 @@
             begin
                 op_val <= 'b1;
                 $display("%M %t - OPERAND VALID SIGNAL ASSERTED", $time);
-                @(negedge res_ready);
+                @(negedge op_ready);
                 op_val <= 'b0;
                 $display("%M %t - OPERAND VALID SIGNAL DEASSERTED", $time);
             end    
