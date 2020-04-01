479f57eb054b14323e690362a9b1ea5e3b4ff321
gheorghestefan996 <gheorghe.stefan996@gmail.com>
2020-04-01 18:18:22 +0300

    modified cod after course suggestions

--- Multiplicator numere complexe/complex_nr_multiplier_1_instance/sim/work/control_logic/_primary.vhd
+++ Multiplicator numere complexe/complex_nr_multiplier_1_instance/sim/work/control_logic/_primary.vhd
@@ -1,16 +1,6 @@
 library verilog;
 use verilog.vl_types.all;
 entity control_logic is
-    generic(
-        IDLE            : integer := 0;
-        LOAD_OPERANDS   : integer := 2;
-        MULT_RE_X_RE    : integer := 1;
-        MULT_IM_X_IM    : integer := 7;
-        MULT_RE_X_IM_1  : integer := 5;
-        MULT_RE_X_IM_2  : integer := 3;
-        COMPUTE_RESULT  : integer := 4;
-        WAIT_RESULT_RDY : integer := 6
-    );
     port(
         clk             : in     vl_logic;
         rstn            : in     vl_logic;
--- Multiplicator numere complexe/complex_nr_multiplier_1_instance/src/rtl/control_logic.v
+++ Multiplicator numere complexe/complex_nr_multiplier_1_instance/src/rtl/control_logic.v
@@ -9,23 +9,23 @@ module control_logic(
     input                   op_val        , // data valid signal
     input                   res_ready     , // the consumer is ready to receive the result
 
-    output wire             op_ready          , // module is ready to receive new operands
-    output wire             res_val           , // result valid signal
-    output wire             op_1_sel          , // Selection signal for the first operand of the uint8_mult module
-    output wire             op_2_sel          , // Selection signal for the second operand of the uint8_mult module
-    output wire             compute_enable    , // enable for final result computation
-    output wire [1:0]       result_reg_sel      // Selection signal for destination register of the results
+    output             op_ready          , // module is ready to receive new operands
+    output             res_val           , // result valid signal
+    output             op_1_sel          , // Selection signal for the first operand of the uint8_mult module
+    output             op_2_sel          , // Selection signal for the second operand of the uint8_mult module
+    output             compute_enable    , // enable for final result computation
+    output reg [1:0]   result_reg_sel      // Selection signal for destination register of the results
 );
 
     //State parameters
-    parameter IDLE              = 3'b000; // Idle state
-    parameter LOAD_OPERANDS     = 3'b010; // Wait 1 clock cycle to load the operands in the registers when data valid is asserted
-    parameter MULT_RE_X_RE      = 3'b001; // Multiply the real part of the operands
-    parameter MULT_IM_X_IM      = 3'b111; // Multiply the imaginary part of the operands
-    parameter MULT_RE_X_IM_1    = 3'b101; // Multiply the real part of the first operand with the imaginary part of the second operand
-    parameter MULT_RE_X_IM_2    = 3'b011; // Multiply the real part of the second operand with the imaginary part of the firs operand
-    parameter COMPUTE_RESULT    = 3'b100; // Compute final result
-    parameter WAIT_RESULT_RDY   = 3'b110; // Wait for result ready signal to be asserted
+    localparam IDLE              = 3'b000; // Idle state
+    localparam LOAD_OPERANDS     = 3'b010; // Wait 1 clock cycle to load the operands in the registers when data valid is asserted
+    localparam MULT_RE_X_RE      = 3'b001; // Multiply the real part of the operands
+    localparam MULT_IM_X_IM      = 3'b111; // Multiply the imaginary part of the operands
+    localparam MULT_RE_X_IM_1    = 3'b101; // Multiply the real part of the first operand with the imaginary part of the second operand
+    localparam MULT_RE_X_IM_2    = 3'b011; // Multiply the real part of the second operand with the imaginary part of the firs operand
+    localparam COMPUTE_RESULT    = 3'b100; // Compute final result
+    localparam WAIT_RESULT_RDY   = 3'b110; // Wait for result ready signal to be asserted
 
     // Internal signals and registers
     reg [2:0] state;
@@ -71,11 +71,17 @@ module control_logic(
     
     assign op_ready         = state == IDLE;                                                // The module is ready to receive new operands only in IDLE state
     assign res_val          = state == WAIT_RESULT_RDY;                                     // Result is computed and result valid signal is asserted  
-    assign op_1_sel         = (state[0] == 'b1)? state[2] : 'bx;                            // 0 = op 1 re, 1 = op 1 im 
-    assign op_2_sel         = (state[0] == 'b1)? state[1] : 'bx;                            // 0 = op 2 re, 1 = op 2 im
+    assign op_1_sel         = state == MULT_IM_X_IM | state == MULT_RE_X_IM_2;              // 0 = op 1 re, 1 = op 1 im 
+    assign op_2_sel         = state == MULT_IM_X_IM | state == MULT_RE_X_IM_1;              // 0 = op 2 re, 1 = op 2 im
     assign compute_enable   = state == COMPUTE_RESULT;                                      // Module is ready for final computation 
 
-    assign result_reg_sel   = (state[0] == 'b1)? {(state[2]^state[1]),state[1]} : 'bx;      // Select where the multiply partial result is stored            
-
+    always @(*)                                                                             // Select where the multiply partial result is stored            
+    case (state)
+        MULT_RE_X_RE  :     result_reg_sel <= 2'b00;   
+        MULT_IM_X_IM  :     result_reg_sel <= 2'b01; 
+        MULT_RE_X_IM_1:     result_reg_sel <= 2'b10; 
+        MULT_RE_X_IM_2:     result_reg_sel <= 2'b11;  
+        default: result_reg_sel <= 2'b00;  
+    endcase
 
 endmodule // control_logic
