1df32a7e40c27d70b7ae81701da2906648854db7
gheorghestefan996 <gheorghe.stefan996@gmail.com>
2020-11-06 12:14:42 +0200

    last commit

--- Multiplicator numere complexe/complex_nr_multiplier_1_instance/sim/work/complex_nr_mult_tb/_primary.vhd
+++ Multiplicator numere complexe/complex_nr_multiplier_1_instance/sim/work/complex_nr_mult_tb/_primary.vhd
@@ -3,16 +3,15 @@ use verilog.vl_types.all;
 entity complex_nr_mult_tb is
     generic(
         DATA_WIDTH      : integer := 8;
-        TEST_SCENARIO   : integer := 0
+        TEST_SCENARIO   : integer := 0;
+        TRANSACTION_NR  : integer := 20
     );
     port(
         clk             : in     vl_logic;
         rstn            : in     vl_logic;
         op_ready        : in     vl_logic;
-        res_val         : in     vl_logic;
         sw_rst          : out    vl_logic;
         op_val          : out    vl_logic;
-        res_ready       : out    vl_logic;
         op_data         : out    vl_logic_vector
     );
 end complex_nr_mult_tb;
--- Multiplicator numere complexe/complex_nr_multiplier_1_instance/sim/work/test_environment/_primary.vhd
+++ Multiplicator numere complexe/complex_nr_multiplier_1_instance/sim/work/test_environment/_primary.vhd
@@ -4,8 +4,10 @@ entity test_environment is
     generic(
         DATA_WIDTH      : integer := 8;
         CLOCK_PERIOD    : integer := 5;
+        TRANSACTION_NR  : integer := 30;
         RST_DELAY       : integer := 30;
         RST_DURATION    : integer := 2;
-        TEST_SCENARIO   : integer := 0
+        TEST_SCENARIO   : integer := 3;
+        WAIT_BEFORE_READY: integer := 20
     );
 end test_environment;
